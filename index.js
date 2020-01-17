4e2aea22a5df01cb3cfdcf6289cf13fc77de55f7
Recep Öztürk <turkishteacher23@gmail.com>
2020-01-17 12:25:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+document.querySelector("main").remove();
+let newHeader = document.createElement("h1");
+newHeader.id="victory";
+document.body.appendChild(newHeader);
+
+// let newHeader = document.querySelector("h1#victor");
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
