e04850a19264c075ce93b627aff1ad7912fd7be2
learn <is4syria@gmail.com>
2020-01-21 07:13:44 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+main.remove();
+
+let h1 = document.createElement('h1')
+h1.id="victory";
+document.body.appendChild(h1);
+
+let newHeader = document.querySelector("h1#victory");
+newHeader.innerHTML = "YOUR-NAME is the champion";
