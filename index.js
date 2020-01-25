f3c5f22f3b0e6f7daf1723d20978201be7088936
Ammar <ammar.n.sammour@gmail.com>
2020-01-25 20:05:22 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById("main").remove()
+
+const newHeader = document.createElement("h1")
+
+newHeader.setAttribute("id", "victory")
+
+newHeader.innerHTML = "Ammar is the champion"
