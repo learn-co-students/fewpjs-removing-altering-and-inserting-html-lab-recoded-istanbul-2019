81d2e903e0cd31ba79d6dbd0278ca71e35a1fd63
Hakan Yildiz <hakan@re-coded.com>
2020-01-29 13:13:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.querySelector('main#main').remove(); //to remove the main 
+
+let newHeader = document.createElement('h1'); //to create a h1 element
+
+newHeader.setAttribute('id', 'victory');     //to give an id attribute to the h1 element with 'victory'
+
+newHeader.innerHTML = 'iHakan is the champion'; //to add the message inside the h1 tag
+
+document.body.appendChild('newHeader');     //to see it on the DOM
