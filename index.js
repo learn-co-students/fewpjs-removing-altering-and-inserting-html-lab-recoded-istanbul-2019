360c88fb68d67ab39332db2bcad026dd3bb2b752
Halit Batur <halitfuatbatur@gmail.com>
2020-04-03 20:03:31 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.querySelector('#main').remove();
+
+let newHeader = document.createElement('h1');
+
+newHeader.id='victory';
+
+console.log(newHeader);
+
+newHeader.innerHTML = 'YOUR-NAME is the champion';
