1e158b8f5bc9f22d5931c6884295e3e9a7479863
mira_arar <mira.ar230@gmail.com>
2020-04-04 23:35:53 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.getElementById('main').remove();
+// 2
+var newHeader = document.createElement('h1');
+newHeader.nodeName;
+// 3
+newHeader.id="victory";
+//4 
+newHeader.innerHTML = 'is the champion';
+console.log(newHeader);
