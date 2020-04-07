928faebd3f2c70196995442e9af9536ba57b1b14
Majd-07 <m.shoghri07@gmail.com>
2020-04-07 22:53:22 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.getElementById('main').remove();
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+document.body.appendChild(newHeader);
+newHeader.innerHTML = 'MAJD is the champion';
