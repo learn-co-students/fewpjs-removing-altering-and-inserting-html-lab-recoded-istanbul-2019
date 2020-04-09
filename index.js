32270bd1625104bb3954b3952b050d90166f108d
Dara <dara.dandashi@gmail.com>
2020-04-09 17:42:00 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.getElementById('main').remove();
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id",'victory');
+newHeader.textContent = "Dara is the champion"
