345cfc7527028a0f8bb09b0458f65c71ad39254b
tugba <ttugbasen@gmail.com>
2020-04-10 22:28:33 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+let main = document.getElementById('main')
+main.remove()
+let newHeader = document.getElementById('victory')
+newHeader.innerHTML = " Tugba is the champion" 
+
