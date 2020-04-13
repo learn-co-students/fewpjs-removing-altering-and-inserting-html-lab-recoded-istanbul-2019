2d624c4fea2f0ec2fdadf60d603fb0c07cad57d3
Maher Suleyman <maher.suleyman.ms@gmail.com>
2020-04-13 03:53:40 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+// 1st task
+document.body.removeChild(document.querySelector('#main'));
+
+// 2nd task
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+document.body.appendChild(newHeader);
+
+// 3rd task
+newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+document.body.appendChild(newHeader);
+
+// 4th task
+newHeader = document.createElement('h1');
+newHeader   .setAttribute("id", "victory");
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "Mahir is the champion";
